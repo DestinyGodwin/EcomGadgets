12987f63ab62087f4d44ad3f559198d9b2f62777
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-28 06:06:46 +0100

    updated store migration

--- database/migrations/2025_06_28_050438_create_stores_table.php
+++ database/migrations/2025_06_28_050438_create_stores_table.php
@@ -12,7 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('stores', function (Blueprint $table) {
-            $table->id();
+        $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
+            $table->string('store_name');
+            $table->string('slug')->unique();
+            $table->text('store_description');
+            $table->string('store_image')->nullable();
+            $table->string('phone');
+            $table->string('email');
+            $table->foreignUuid('state_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignUuid('lga_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('address')->nullable();
+            $table->timestamp('subscription_expires_at')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
