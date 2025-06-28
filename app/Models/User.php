fb31c32a308629207cf633a0a0270f0b30341459
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-28 05:59:35 +0100

    updated user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,24 +3,32 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasUuids, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
+       protected $fillable = [
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'phone',
+        'profile_picture',
+        'state_id',
+        'lga_id',
     ];
 
     /**
@@ -44,5 +52,25 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function store()
+    {
+        return $this->hasOne(Store::class);
+    }
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function lga(): BelongsTo
+    {
+        return $this->belongsTo(Lga::class);
+    }
+    public function isVendor(){
+        return $this->role === 'vendor';
     }
 }
