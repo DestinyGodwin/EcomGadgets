55c4d24eaeca9d1f8b2f273103255f4e737fe1c9
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-28 13:01:29 +0100

    updated featured payment log migration

--- database/migrations/2025_06_28_114547_create_featured_product_logs_table.php
+++ database/migrations/2025_06_28_114547_create_featured_product_logs_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('featured_product_logs', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('store_id')->constrained()->onDelete('cascade');
+            $table->foreignId('plan_id')->constrained('featured_product_plans');
+            $table->string('reference')->unique();
+            $table->timestamp('starts_at');
+            $table->timestamp('ends_at');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
