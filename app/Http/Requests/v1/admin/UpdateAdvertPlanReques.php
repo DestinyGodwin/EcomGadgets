1bd6f5b7000897beda572c62c385a9270e1e1b37
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-28 09:35:47 +0100

    added update  advert plan request

---
+++ app/Http/Requests/v1/admin/UpdateAdvertPlanReques.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\V1\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAdvertPlanReques extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+       return [
+            'name' => ['sometimes', 'string', 'max:255'],
+            'price' => ['sometimes', 'numeric', 'min:0'],
+            'description' => ['sometimes', 'string', 'min:3', 'max:255'],
+            'duration_days' => ['sometimes', 'integer', 'min:1'],
+        ];
+}
