fdb49adef83811569e6fda0f979049799ab8e45f
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-28 12:27:25 +0100

    added payment service class

---
+++ app/Services/V1/PaymentService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services\V1;
+
+use App\Models\Transaction;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
+
+class PaymentService
+{
+    /**
+     * Create a new class instance.
+     */
+     public function initialize(array $data): array
+    {
+        $reference = 'PS_' . Str::uuid();
+
+        // Create pending transaction
+        Transaction::create([
+            'reference' => $reference,
+            'user_id'   => $data['user_id'],
+            'type'      => $data['type'],
+            'amount'    => $data['amount'],
+            'status'    => 'pending',
+            'channel'   => 'paystack',
+            'meta'      => $data['metadata'],
+        ]);
+
+        // Hit Paystack
+        $response = Http::withToken(config('services.paystack.secret'))
+            ->post('https://api.paystack.co/transaction/initialize', [
+                'email' => $data['email'],
+                'amount' => $data['amount'] * 100,
+                'reference' => $reference,
+                'metadata' => array_merge($data['metadata'], [
+                    'reference' => $reference,
+                    'payment_type' => $data['type'],
+                ]),
+            ])->json();
+
+        return [
+            'authorization_url' => $response['data']['authorization_url'],
+            'reference' => $reference,
+        ];
+    }
+}
