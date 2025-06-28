1f60bc620b2a315b75f09bd760a8a0e05799c566
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-28 06:13:38 +0100

    added product model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-       use HasUuids, SoftDeletes , HasSlug HasFactory;
+       use HasUuids, SoftDeletes, HasSlug HasFactory;
 
         protected $fillable = [
       'name' ,'slug' ,
---
+++ app/Models/Product.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use App\Models\Scopes\ActiveStoreScope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasUuids, SoftDeletes, HasSlug ;
+
+    protected $fillable = ['category_id', 'name', 'slug', 'description', 'specifications', 'brand', 'price', 'wholesale_price'];
+    public function store(): BelongsTo{
+        return $this->belongsTo(Store::class);
+    }
+    
+     public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+     public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+      public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+    protected $casts = [
+    'specifications' => 'array',
+];
+protected static function booted(): void
+{
+    static::addGlobalScope(new ActiveStoreScope);
+}
+}
