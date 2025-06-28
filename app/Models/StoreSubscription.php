f7c6cd628edaf63b6e5d9844db42201a452e250b
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-28 07:08:27 +0100

    added store subscription model

---
+++ app/Models/StoreSubscription.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class StoreSubscription extends Model
+{
+       use HasUuids, SoftDeletes;
+
+       protected $fillable = ['store_id', 'amount', 'starts_at', 'ends_at', 'transaction_id'];
+
+       protected $hidden =[
+              'laravel_through_key', 'deleted_at', 'updated_at'
+       ];
+
+       public function store(){
+              return $this->belongsTo(Store::class);
+       }
+
+}
