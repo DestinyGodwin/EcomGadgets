4864f72313446ffed416de9359ccb89ccfa7dd9d
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-28 09:33:32 +0100

    added store advert plan request

--- app/Http/Controllers/V1/Admin/FeaturedProductPlanController.php
+++ app/Http/Controllers/V1/Admin/FeaturedProductPlanController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\V1\Admin;
 
-use Illuminate\Http\Request;
 use App\Models\FeaturedProductPlan;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\FeaturedProductPlanResource;
+use App\Http\Requests\V1\Admin\StoreFeaturedProductPlanRequest;
 use App\Http\Requests\V1\Admin\UpdateFeaturedProductPlanRequest;
 
 class FeaturedProductPlanController extends Controller
