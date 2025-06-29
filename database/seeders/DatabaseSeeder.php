553497bd86221577ff3e4532b544079e844a6298
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-29 05:56:26 +0100

    updated db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\SubscriptionPlanSeeder;
+use Database\Seeders\NigeriaStatesLgasSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+         $this->call([
+        NigeriaStatesLgasSeeder::class,
+        SubscriptionPlanSeeder::class,
+        CategorySeeder::class,
+    ]);
     }
 }
