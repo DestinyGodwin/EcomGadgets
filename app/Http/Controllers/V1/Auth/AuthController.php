fe0e1ae379b5220d4f43fb15206d467d185a0924
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-29 03:54:37 +0100

    added auth controller

---
+++ app/Http/Controllers/V1/Auth/AuthController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\V1\Auth;
+
+use Illuminate\Http\Request;
+use App\Services\V1\AuthServices;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\Auth\LoginRequest;
+use App\Http\Requests\V1\Auth\VerifyEmailRequest;
+use App\Http\Requests\V1\Auth\RegisterUserRequest;
+use App\Http\Requests\V1\Auth\ResetPasswordRequest;
+use App\Http\Requests\V1\Auth\UpdateProfileRequest;
+use App\Http\Requests\V1\Auth\ChangePasswordRequest;
+use App\Http\Requests\V1\Auth\ForgotPasswordRequest;
+use App\Http\Requests\V1\Auth\CompleteProfileRequest;
+
+class AuthController extends Controller
+{
+
+    public function __construct(protected AuthServices $authServices){}
+
+    public function store(RegisterUserRequest $request){
+      $data = $this->authServices->store($request->validated());
+
+       return response()->json($data, 201);
+    }
+public function login(LoginRequest $request)
+{
+    $data = $this->authServices->login($request->validated());
+
+    if (isset($data['error'])) {
+        return response()->json(['message' => $data['error']], 401);
+    }
+    return response()->json(['mesage' => 'Logged in successfully', 'token' => $data]);
+}
+
+    public function verifyEmail(VerifyEmailRequest $request){
+        $data = $this->authServices->verifyEmail($request->otp);
+        if (isset($data['error'])){
+            return response()->json(['message' => $data['error']], 401);
+        }
+        return response()->json(['message' => 'Email verified successfully']);
+    }
+
+    public function resendOtp(){
+        $this->authServices->resendOtp();
+        return response()->json('Otp sent');
+    }
+
+    public function completeProfile(CompleteProfileRequest $request){
+      $this->authServices->completeProfile($request->validated());
+      return response()->json('Registration completed successfully');
+    }
+
+    public function updateProfile(UpdateProfileRequest $request){
+        $this->authServices->updateProfile($request->validated());
+        return response()->json('Profile updated successfully');
+    }
+    
+    public function changePassword(ChangePasswordRequest $request){
+        $data = $this->authServices->changePassword($request->validated());
+     if (!$data['success']) {
+            return response()->json(['message' => $data['message']], 422);
+        }
+        return response()->json(['message' => $data['message']], 200);
+    }
+
+    public function forgotPassword(ForgotPasswordRequest $request){
+        $data = $this->authServices->forgotPassword($request->validated());
+        return response()->json($data);
+    }
+
+    public function resetPassword(ResetPasswordRequest $request){
+        $data = $this->authServices->resetPassword($request->validated());
+        if (!$data['success']) {
+            return response()->json(['message' => $data['message']], 422);
+        }
+        return response()->json(['message' => $data['message']], 200);
+    }
+
+    public function getProfile(){
+      $user =   $this->authServices->getProfile();
+        return new UserResource($user);
+    }
+
+    public function logout(Request $request){
+        $this->authServices->logout();
+        return response()->json('logged out successfully');
+    }
+}
