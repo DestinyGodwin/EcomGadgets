a6b8e552a43ee664038a33c464065d231e081e05
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-29 03:33:01 +0100

    added store resource

---
+++ app/Http/Resources/V1/Stores/StoreResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\V1\Stores;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StoreResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'store_name' => $this->store_name,
+            'store_description' => $this->store_description,
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'store_image' => $this->store_image,
+            'address' => $this->address
+
+        ];
+
+    }
+}
