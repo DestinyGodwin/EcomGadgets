20052587dc28b5d533c222829e03dd9079bddd82
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-30 20:06:12 +0100

    added admin notification controller

---
+++ app/Http/Controllers/V1/Admin/NotifyingController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NotifyingController extends Controller
+{
+    protected $notificationService;
+
+    public function __construct(NotificationService $notificationService)
+    {
+        $this->notificationService = $notificationService;
+    }
+
+    public function send(SendNotificationRequest $request)
+    {
+        $data = $request->validated();
+
+        $result = $this->notificationService->sendToUsers(
+            $data['user_ids'] ?? null,
+            $data['subject'],
+            $data['message']
+        );
+
+        return response()->json([
+            'message' => 'Notification sent successfully',
+            'details' => $result,
+        ]);
+    }
+}
