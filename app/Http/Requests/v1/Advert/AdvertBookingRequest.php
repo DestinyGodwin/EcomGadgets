7341856d97e9a5adad7885ad3e01677ec738ca3a
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-30 10:52:33 +0100

    updated advert booking request

--- app/Http/Requests/v1/Advert/AdvertBookingRequest.php
+++ app/Http/Requests/v1/Advert/AdvertBookingRequest.php
@@ -24,7 +24,10 @@ class AdvertBookingRequest extends FormRequest
         return [
             'plan_id' => ['required','exists:advert_plans,id'],
             'state_id' => ['required','exists:states,id'],
-            'starts_at' => ['required', 'date', 'after_or_equal:today'],       
+            'starts_at' => ['required', 'date', 'after_or_equal:today'],  
+             'title'      => ['nullable', 'string', 'max:255'],
+            'image'      => ['required', 'image', 'mimes:jpg,jpeg,png,webp', 'max:2048'],
+            'link'       => ['nullable', 'url'],     
         ];
     }
 }
