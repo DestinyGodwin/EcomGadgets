3f28e5a5c7e638f8842b2a94bc2797f032772624
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-07-01 23:34:36 +0100

    added advert booking controleer

---
+++ app/Http/Controllers/V1/AdvertBookingController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Models\AdvertBooking;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AdvertBookingController extends Controller
+{
+    public function myBookings()
+    {
+        $storeId = Auth::user()->store->id;
+        return AdvertBooking::where('store_id', $storeId)->latest()->get();
+    }
+
+    public function show($id)
+    {
+        $storeId = Auth::user()->store->id;
+        $advert = AdvertBooking::where('id', $id)->where('store_id', $storeId)->firstOrFail();
+
+        return response()->json($advert);
+    }
+
+    public function update(UpdateAdvertBookingRequest $request, $id)
+    {
+        $store = Auth::user()->store;
+        $advert = AdvertBooking::where('id', $id)->where('store_id', $store->id)->firstOrFail();
+
+        if ($advert->ends_at && now()->gt($advert->ends_at)) {
+            return response()->json(['message' => 'This advert has expired and cannot be updated'], 403);
+        }
+
+        $data = $request->validated();
+
+        if ($request->hasFile('image')) {
+            $data['image'] = $request->file('image')->store('advert_images', 'public');
+        }
+
+        $advert->update($data);
+
+        return response()->json(['message' => 'Updated', 'data' => $advert]);
+    }
+}
