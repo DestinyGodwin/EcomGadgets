402825d3d4d0e572ef94115d1520a06b605e4b9f
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-07-03 16:29:22 +0100

    added otp mail class

---
+++ app/Mail/V1/Auth/OtpMail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail\V1\Auth;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OtpMail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+
+    public $otp;
+    public $user;
+
+    public function __construct($otp, $user = null)
+    {
+        $this->otp = $otp;
+        $this->user = $user;
+    }
+
+       public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Your OTP Code',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.auth.otp',
+            with: [
+                'otp' => $this->otp,
+                 'user' => $this->user,
+            ],
+        );
+    }
+  
+}
