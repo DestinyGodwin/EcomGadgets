aaff9a2e69e0c086d8c9e2402da1741bf2398f4b
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-07-09 21:16:50 +0100

    updated review controller

--- app/Http/Controllers/V1/Product/ReviewController.php
+++ app/Http/Controllers/V1/Product/ReviewController.php
@@ -1,33 +1,38 @@
 <?php
-
 namespace App\Http\Controllers\V1\Product;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Services\V1\Product\ReviewService;
-use App\Http\Resources\V1\Product\ReviewResource;
 use App\Http\Requests\V1\Products\StoreReviewRequest;
+use App\Http\Requests\V1\Products\UpdateReviewRequest;
+use App\Http\Resources\V1\Product\ReviewResource;
+use App\Services\V1\Product\ReviewService;
 
 class ReviewController extends Controller
 {
-    public function __construct(protected ReviewService $reviewService) {}
-    
-        public function store(StoreReviewRequest $request)
-        {
-            $review = $this->reviewService->store($request);
-            return new ReviewResource($review);
-        }
-    
-        public function byProduct($productId)
-        {
-            return ReviewResource::collection(
-                $this->reviewService->getByProduct($productId)
-            );
-        }
-    
-        public function destroy($id)
-        {
-            $this->reviewService->delete($id);
-            return response()->json(['message' => 'Review deleted successfully.']);
-        }
+    public function __construct(protected ReviewService $reviewService)
+    {}
+
+    public function store(StoreReviewRequest $request)
+    {
+        $review = $this->reviewService->store($request);
+        return new ReviewResource($review);
+    }
+
+    public function byProduct($productId)
+    {
+        return ReviewResource::collection(
+            $this->reviewService->getByProduct($productId)
+        );
+    }
+    public function update(UpdateReviewRequest $request, $id)
+    {
+        $review = $this->reviewService->update($id, $request);
+        return new ReviewResource($review);
+    }
+
+    public function destroy($id)
+    {
+        $this->reviewService->delete($id);
+        return response()->json(['message' => 'Review deleted successfully.']);
+    }
 }
