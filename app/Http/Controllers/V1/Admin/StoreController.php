c2110f87ff2c4831451773f5cf42bbbd023d752f
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-07-10 10:38:12 +0100

    added store reactivation notification

--- app/Http/Controllers/V1/Admin/StoreController.php
+++ app/Http/Controllers/V1/Admin/StoreController.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Http\Controllers\V1\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Models\Store;
-use App\Notifications\V1\Stores\StoreApprovedNotification;
-use App\Notifications\V1\Stores\StoreDeactivatedNotification;
-use App\Notifications\V1\Stores\StoreDeclinedNotification;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Notifications\V1\Stores\StoreApprovedNotification;
+use App\Notifications\V1\Stores\StoreDeclinedNotification;
+use App\Notifications\V1\Stores\StoreDeactivatedNotification;
+use App\Notifications\V1\Stores\StoreReactivatedNotification;
 
 class StoreController extends Controller
 {
--- app/Notifications/V1/Stores/StoreReactivatedNotification.php
+++ app/Notifications/V1/Stores/StoreReactivatedNotification.php
@@ -8,14 +8,14 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class StoreDeclinedNotification extends Notification implements ShouldQueue
+class StoreReactivatedNotification extends Notification implemnts ShouldQueue
 {
     use Queueable;
 
     /**
      * Create a new notification instance.
      */
-  public function __construct(protected Store $store, protected string $reason) {}
+   public function __construct(protected Store $store,protected string $message ) {}
 
     public function via($notifiable)
     {
@@ -25,13 +25,15 @@ class StoreDeclinedNotification extends Notification implements ShouldQueue
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->subject('Your Store Registration Was Declined')
+            ->subject('Your Store Has Been Reactivated')
             ->greeting("Hello {$notifiable->first_name},")
-            ->line("Your store \"{$this->store->store_name}\" was declined.")
-            ->line('Reason: ' . $this->reason)
-            ->action('Resubmit Your Store', url("/store/edit/{$this->store->slug}"))
-            ->line('Please update your store details and resubmit for review.');
+            ->line("We're glad to let you know that your store \"{$this->store->store_name}\" has been reactivated.")
+            ->line('Admin Message: ' . $this->message)
+            ->action('Manage Your Store', url("/store/edit/{$this->store->slug}"))
+            ->line('Thank you for working with us!');
     }
+
+
     /**
      * Get the array representation of the notification.
      *
