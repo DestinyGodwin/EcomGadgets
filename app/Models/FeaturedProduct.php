4d90e60b6319a25c5f2d712c0b1ac47dc9a0e929
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-08-04 14:04:01 +0100

    updated featured product model

--- app/Models/FeaturedProduct.php
+++ app/Models/FeaturedProduct.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class FeaturedProduct extends Model {
     use HasUuids;
+
+      protected $fillable = [
+        'product_id',
+        'store_id',
+        'plan_id',
+        'starts_at',
+        'ends_at',
+    ];
+
+    protected $casts = [
+        'starts_at' => 'datetime',
+        'ends_at'   => 'datetime',
+    ];
+
+    public function product() { return $this->belongsTo(Product::class); }
+
+    public function plan() { return $this->belongsTo(FeaturedProductPlan::class); }
+
+    public function store() { return $this->belongsTo(Store::class); }
+
 }
