89c8dbba0aa1d198f53b89c5833984b8560838f4
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-08-05 00:45:34 +0100

    added featured_subscription_products

---
+++ database/migrations/2025_08_05_004426_create_featured_subscription_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('featured_subscription_products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('subscription_id')->constrained('featured_product_subscriptions')->onDelete('cascade');
+            $table->foreignUuid('product_id')->constrained()->onDelete('cascade');
+            $table->timestamp('added_at')->default(now());
+            $table->timestamps();
+
+            $table->unique(['subscription_id', 'product_id']);
+            $table->index('subscription_id');
+            $table->index('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('featured_subscription_products');
+    }
+};
