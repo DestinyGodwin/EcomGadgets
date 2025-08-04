a76399f23ccd3a0ab5d29a5e39352431d621d026
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-08-04 23:37:24 +0100

    updated featured product model

--- app/Models/FeaturedProductPlan.php
+++ app/Models/FeaturedProductPlan.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FeaturedProductPlan extends Model {
     use HasUuids, SoftDeletes;
@@ -13,5 +14,24 @@ class FeaturedProductPlan extends Model {
         'name', 'price', 'description', 'duration_days',    'max_products', 'refresh_interval_minutes'
 
     ];
+
+      public function subscriptions(): HasMany
+    {
+        return $this->hasMany(FeaturedProductSubscription::class, 'plan_id');
+    }
+
+    public function activeSubscriptions(): HasMany
+    {
+        return $this->subscriptions()->where('is_active', true)
+            ->where(function ($query) {
+                $query->whereNull('ends_at')
+                      ->orWhere('ends_at', '>', now());
+            });
+    }
+
+    public static function getPlansOrderedByPrice()
+    {
+        return self::orderByDesc('price')->get();
+    }
 }
 
