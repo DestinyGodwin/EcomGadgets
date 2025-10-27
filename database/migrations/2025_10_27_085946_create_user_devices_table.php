1c4d0ed776bc57ada29663cdf61830ba14245c2a
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-10-27 10:08:28 +0100

    updated user device migration

--- database/migrations/2025_10_27_085946_create_user_devices_table.php
+++ database/migrations/2025_10_27_085946_create_user_devices_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_devices', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('device_token')->unique();
+            $table->string('device_name')->nullable();
             $table->timestamps();
         });
     }
