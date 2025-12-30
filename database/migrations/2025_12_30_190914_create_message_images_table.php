44c992938fff0fa48438b535c38f62ac750eaa08
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-12-30 20:16:35 +0100

    updated message media migration

--- database/migrations/2025_12_30_190914_create_message_images_table.php
+++ database/migrations/2025_12_30_190914_create_message_images_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('message_images', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('product_id')->constrained()->cascadeOnDelete();
+            $table->string('image_path');
             $table->timestamps();
         });
     }
