6be56145d2a8354ef5ae9bf04136bcec29ab0ed4
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-12-30 06:17:38 +0100

    updated converation migration

--- database/migrations/2025_12_30_050136_create_conversations_table.php
+++ database/migrations/2025_12_30_050136_create_conversations_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('conversations', function (Blueprint $table) {
-            $table->id();
+             $table->uuid('id')->primary();
+            $table->timestamp('last_message_at')->nullable()->index();
             $table->timestamps();
         });
     }
