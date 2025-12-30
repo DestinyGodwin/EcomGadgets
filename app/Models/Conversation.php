8386e192b017f3b40db9bf8a3cedd69b7f135de6
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-12-30 06:03:49 +0100

    added conversation model and migration

---
+++ app/Models/Conversation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    use HasUuids;
+
+     protected $fillable = ['last_message_at'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)
+            ->withTimestamps();
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+}
