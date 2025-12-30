7a903b390a47999cb1621623678283d23f66aefa
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-12-30 22:13:24 +0100

    updated channel

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\Conversation;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });
+
+
+Broadcast::channel('conversation.{conversationId}', function ($user, $conversationId) {
+    return Conversation::where('id', $conversationId)
+        ->whereHas('users', fn ($q) => $q->where('user_id', $user->id))
+        ->exists();
 });
