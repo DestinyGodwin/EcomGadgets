a991de1049fcc5bfa039faf0b71ffaebd962c448
DestinyGodwin <destinyogodwin.dg@gmail.com>
2026-01-12 05:12:18 +0100

    fixed conversations

--- app/Http/Controllers/V1/Chat/ConversationController.php
+++ app/Http/Controllers/V1/Chat/ConversationController.php
@@ -14,8 +14,8 @@ class ConversationController extends Controller
         return $request->user()
             ->conversations()
             ->with([
-                'users:id,name',
-                'lastMessage.sender:id,name',
+                'users:id,first_name,last_name',
+                'lastMessage.sender:id,first_name',
             ])
             ->orderByDesc('last_message_at')
             ->paginate(20);
