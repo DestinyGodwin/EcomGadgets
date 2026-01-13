90cfbd4da78d48872161b1c581850b09be950839
DestinyGodwin <destinyogodwin.dg@gmail.com>
2026-01-13 04:30:36 +0100

    fixed messages

--- app/Http/Controllers/V1/Chat/MessageController.php
+++ app/Http/Controllers/V1/Chat/MessageController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\V1\Chat;
 
-use App\Models\Conversation;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Services\V1\Chat\ChatService;
-use App\Http\Resources\V1\Chat\MessageResource;
 use App\Http\Requests\V1\Chat\SendMessageRequest;
+use App\Http\Resources\V1\Chat\MessageResource;
+use App\Models\Conversation;
+use App\Services\V1\Chat\ChatService;
+use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-     public function __construct(private readonly ChatService $chat) {}
+    public function __construct(private readonly ChatService $chat) {}
 
     public function store(SendMessageRequest $request)
     {
@@ -31,14 +31,15 @@ class MessageController extends Controller
 
         return response()->json($message->load('media'), 201);
     }
-public function index(Request $request, Conversation $conversation)
-{
-    $messages = $conversation->messages()
-        ->with(['sender:id,first_name', 'conversation.users:id'])
-        ->latest()
-        ->paginate(30);
 
-    return MessageResource::collection($messages)
-        ->additional(['auth_user_id' => $request->user()->id]);
-}
+    public function index(Request $request, Conversation $conversation)
+    {
+        $messages = $conversation->messages()
+            ->with(['sender:id,first_name', 'conversation.users:id'])
+            ->latest()
+            ->paginate(30);
+
+        return MessageResource::collection($messages)
+            ->additional(['auth_user_id' => $request->user()->id]);
+    }
 }
