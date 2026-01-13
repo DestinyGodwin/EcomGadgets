0bf5e7728141b51ca766e83b350a48aa31e15ec2
DestinyGodwin <destinyogodwin.dg@gmail.com>
2026-01-13 17:54:19 +0100

    updated conversations

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -11,6 +11,11 @@ class Conversation extends Model
 
      protected $fillable = ['last_message_at'];
 
+     protected $casts = [
+    'last_message_at' => 'datetime',
+];
+
+
     public function users()
     {
         return $this->belongsToMany(User::class)
